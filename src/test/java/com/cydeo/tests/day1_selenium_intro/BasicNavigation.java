ebafbb4863cd9e4e3f16e484699af8fcd4c070df
arstanbektemirov <abektemirov82@gmail.com>
2022-02-01 15:37:22 -0500

    first day

--- src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigation.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigation.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         //1-Setup the browser driver
         WebDriverManager.chromedriver().setup();
@@ -15,6 +15,37 @@ public class BasicNavigation {
         //this is the line opening an empty browser
         WebDriver driver = new ChromeDriver();
 
+        //3-Go to "https://www.tesla.com"
         driver.get("https://www.tesla.com");
+        String currentTitle = driver.getTitle();
+        System.out.println("currentTitle= "+currentTitle);
+
+        //Stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        //use selenium to navigate back
+        driver.navigate().back();
+
+        //Stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        //use selenium to navigate forward
+        driver.navigate().forward();
+
+        //Stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        //use selenium to navigate refresh
+        driver.navigate().refresh();
+
+        //use navigate().to().
+        driver.navigate().to("https://www.google.com");
+        currentTitle= driver.getTitle();
+
+        // get the title of the page
+       // System.out.println("driver.getTitle() "+driver.getTitle());
+
+
+        System.out.println("currentTitle= "+currentTitle);
     }
 }
