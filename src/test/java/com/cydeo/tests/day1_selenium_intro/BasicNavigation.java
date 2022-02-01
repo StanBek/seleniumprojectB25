3fddd9e2b675bd8403c4e4140763c2a9d347915d
arstanbektemirov <abektemirov82@gmail.com>
2022-02-01 14:45:30 -0500

    first day

--- src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigation.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigation.java
@@ -1,4 +1,20 @@
 package com.cydeo.tests.day1_selenium_intro;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BasicNavigation {
+
+    public static void main(String[] args) {
+
+        //1-Setup the browser driver
+        WebDriverManager.chromedriver().setup();
+
+        //2-Create instance of the Selenium WebDriver
+        //this is the line opening an empty browser
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.tesla.com");
+    }
 }
