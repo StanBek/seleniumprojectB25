8863be2f9b233a063e618343f6edb63956f14129
arstanbektemirov <abektemirov82@gmail.com>
2022-02-03 20:49:33 -0500

    commit

---
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/test1.java
@@ -0,0 +1,10 @@
+package com.cydeo.tests.day1_selenium_intro;
+
+public class test1 {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+        //test
+
+    }
+}
