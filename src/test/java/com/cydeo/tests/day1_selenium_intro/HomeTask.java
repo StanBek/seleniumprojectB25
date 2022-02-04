97cb80f93734af5a13d58444d6f5e9c0c78f6377
arstanbektemirov <abektemirov82@gmail.com>
2022-02-03 20:48:16 -0500

    first day

---
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/HomeTask.java
@@ -0,0 +1,38 @@
+package com.cydeo.tests.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomeTask {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://practice.cydeo.com");
+
+        String actualURL= driver.getCurrentUrl();
+        if(actualURL.contains("cydeo")){
+            System.out.println("passed");
+        }else{
+            System.out.println("failed");
+        }
+        System.out.println(actualURL);
+
+        String actualTitle = driver.getTitle();
+
+        if(actualTitle.contains("Practice")){
+            System.out.println("Passed");
+        }else{
+            System.out.println("Failed");
+        }
+        System.out.println(actualTitle);
+
+        Thread.sleep(2000);
+
+        driver.close();
+
+
+    }
+}
