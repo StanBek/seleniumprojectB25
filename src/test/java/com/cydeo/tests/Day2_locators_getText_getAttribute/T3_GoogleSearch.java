9f330f5ab5f1c7aa12ce305774ed3fbfb234d43d
arstanbektemirov <abektemirov82@gmail.com>
2022-02-04 14:38:26 -0500

    day_2_T3_GoogleSearch

--- src/test/java/com/cydeo/tests/Day2_locators_getText_getAttribute/T3_GoogleSearch.java
+++ src/test/java/com/cydeo/tests/Day2_locators_getText_getAttribute/T3_GoogleSearch.java
@@ -2,6 +2,7 @@ package com.cydeo.tests.Day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -21,6 +22,8 @@ public class T3_GoogleSearch {
 
         //3- Write “apple” in search box
         WebElement googleSearchBox = driver.findElement(By.name("q"));
+        googleSearchBox.sendKeys("apple"+ Keys.ENTER);
+
 
         //4- Click google search button
         //5- Verify title:
