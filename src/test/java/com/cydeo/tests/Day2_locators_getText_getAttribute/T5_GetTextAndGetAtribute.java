da024106b4cdd3fb24032f35048c1a50774a8ffd
arstanbektemirov <abektemirov82@gmail.com>
2022-02-04 20:25:47 -0500

    day_2_T3_GoogleSearch

--- src/test/java/com/cydeo/tests/Day2_locators_getText_getAttribute/T5_GetTextAndGetAtribute.java
+++ src/test/java/com/cydeo/tests/Day2_locators_getText_getAttribute/T5_GetTextAndGetAtribute.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.Day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T5_GetTextAndGetAtribute {
@@ -18,9 +20,29 @@ public class T5_GetTextAndGetAtribute {
         driver.get("https://practice.cydeo.com/registration_form");
 
         //3- Verify header text is as expected:
-       // Expected: Registration form
-       // 4- Locate “First name” input box
-       // 5- Verify placeholder attribute’s value is as expected:
-        //Expected: first name
+        WebElement headerText = driver.findElement(By.tagName("h2"));
+        String expectedHeaderText = "Registration form";
+        String actualHeaderText = headerText.getText();
+        if (actualHeaderText.equals(expectedHeaderText)) {
+            System.out.println("Verify PASSED!!!");
+        } else {
+            System.out.println("Verify FAILED!!!");
+        }
+
+        System.out.println("==================================");
+        // Expected: Registration form
+        // 4- Locate “First name” input box
+        WebElement inputBox = driver.findElement(By.name("firstname"));
+        String ExpectedInputBox = "first name";
+        String actualInputBox = inputBox.getAttribute("placeholder");
+        if (actualInputBox.equals(ExpectedInputBox)) {
+            System.out.println("Verify PASSED!!!");
+        } else {
+            System.out.println("Verify FAILED!!!");
+
+            driver.close();
+            // 5- Verify placeholder attribute’s value is as expected:
+            //Expected: first name
+        }
     }
 }
