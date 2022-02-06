9a914188cde63bac4f31699e6e1a8a5059830185
arstanbektemirov <abektemirov82@gmail.com>
2022-02-05 19:58:35 -0500

    HomeWork

---
+++ src/test/java/com/cydeo/tests/HomeWork/Test4.java
@@ -0,0 +1,41 @@
+package com.cydeo.tests.HomeWork;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test4 {
+    public static void main(String[] args) {
+
+        /*TC #4: Practice Cydeo – Class locator practice
+        1- Open a chrome browser
+        2- Go to: https://practice.cydeo.com/inputs
+        3- Click to “Home” link
+        4- Verify title is as expected:
+        Expected: Practice
+
+        PS: Locate “Home” link using “className” locator
+
+         */
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://practice.cydeo.com/inputs");
+        WebElement home = driver.findElement(By.linkText("Home"));
+        home.click();
+        String actualTitle= driver.getTitle();
+        String expectedTitle="Practice";
+        if(actualTitle.contains(expectedTitle)){
+            System.out.println("PASSED!!!");
+        }else {
+            System.out.println("FAILED!!!");
+        }
+
+       // driver.close();
+
+    }
+
+
+}
